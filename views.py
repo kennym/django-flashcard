6e75f95d8ead37c68bc2587ccdc97ddce509e45f
Kenny Meyer <knny.myer@gmail.com>
2010-10-26 14:11:39 -0300

    changed template directory structure

--- views.py
+++ views.py
@@ -6,7 +6,7 @@ from django.template import RequestContext
 from dvoc.models import FlashCard
 
 
-def list_flashcards(request, template_name='dvoc/list_flashcards.html'):
+def list_flashcards(request, template_name='list_flashcards.html'):
     """
     Return a list with flash cards.
     """
@@ -18,7 +18,7 @@ def list_flashcards(request, template_name='dvoc/list_flashcards.html'):
     return render_to_response(template_name, return_dict)
 
 @csrf_protect
-def create_flashcard(request, template_name='dvoc/create_flashcard.html'):
+def create_flashcard(request, template_name='create_flashcard.html'):
     """
     Return a form for creating a flashcard.
     """
