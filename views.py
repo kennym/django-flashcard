7dfdf969fab221d22c1646a9dbfea122fea582ca
Kenny Meyer <knny.myer@gmail.com>
2010-10-25 21:50:38 -0300

    Fix typo

--- views.py
+++ views.py
@@ -7,7 +7,8 @@ def flashcard_list(request, template_name='dvoc/list_flashcards.html'):
     Return a list with flash cards.
     """
     flashcard_list = FlashCard.objects.all()
-    return render_to_response(template_name, {'flascard_list': flashcard_list})
+    return render_to_response(template_name,
+                              {'flashcard_list': flashcard_list})
 
 def create_flashcard(request, template_name='dvoc/create_flashcard.html'):
     """
